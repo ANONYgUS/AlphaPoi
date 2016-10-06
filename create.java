3b5710a8a1d3a6ae973cd98562dcf6e699a8ac94
Niklas <nkniesche1@gmail.com>
2016-10-05 23:44:43 -0400

    updated create.java

--- create.java
+++ create.java
@@ -16,16 +16,79 @@ class  create
   public static void main(String[] args) throws Exception
   {
 	//get indexes of where each Student's information starts in the csv file (searched for " character)
-	ArrayList indexes = new ArrayList<Int>();
+	ArrayList indexesStudent = new ArrayList<Int>();
     for row in csvfile{
 		if row has "\""{
 			indexes.add(csvfile row)
 		} 
 	}
 	
-	student
-		weekdays
-			slots
+	Scanner s = new Scanner(pathToCSVFile);
+	
+	for(int i = 0; i<indexesStudent.size(); i++){
+		int start = indexesStudent.get(i);
+		int end = indexesStudent.get(i+1);
+		
+		//a String containing the block of information for each student
+		String infoStudent;
+		for(k = 0; k < (end-start); k++){
+			infoStudent += s.nextLine()+"\n"
+		}
+		
+		String infoMonday = infoStudent.substring(infoStudent.indexOf("Monday")+74, infoStudent.indexOf("Tuesday");
+		Scanner m = new Scanner(infoMonday);
+		
+		//how many courses on Monday
+		ArrayList infoMondayByString = new ArrayList<String>();
+		
+		while(m.hasNextLine()){
+			infoMondayByString.add(m.nextLine());
+		}
+		
+		for(String currentLine: infoMondayByString){
+			String[] input = currentLine.split(",");
+            className = input[0];
+            startTime = input[1];
+            endTime = input[2];
+			location = input[3];
+			slot = input[4];
+			teacher = input[5];
+		}
+		
+		
+		
+	}
+	
+	
+	
+	Student class
+		every student object has 5 instance variables, for each weekday.
+		Day monday
+		Day tuesday
+		Day wednesday
+		SDay thursday
+		Day friday
+	
+	Day class
+		every day has 7 instance variables, for each class
+		String class1
+		String class2
+		String class3
+		String class4
+		String class5
+		String class6
+		String class7
+	
+	SDay class
+		every day has 8 instance variables, for each class
+		String class1
+		String class2
+		String class3
+		String class4
+		String class5
+		String class6
+		String class7
+		String class8
     
   }
   public static void writeToCell(int r, int c, String str) throws Exception{
