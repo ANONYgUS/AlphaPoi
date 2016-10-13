95a4669f874d6a7b32e66e969b44ead46ef3eeca
Niklas <nkniesche1@gmail.com>
2016-10-13 09:12:50 -0400

    new structure and stuff

--- AlphaPoi_DRV.java
+++ AlphaPoi_DRV.java
@@ -1,15 +1,11 @@
 import java.util.*;
 import java.io.*;
-/**
- * Write a description of class Organize here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class AlphaPoi_DRV
 {
     public static void main(String[] args) throws Exception{
         AlphaPoi obj = new AlphaPoi("studentSchedules16_2.csv");
-        obj.printClassSchedule("11");
+        obj.addCourse("Kniesche, Niklas", "Honors Chemistry");
+        obj.printStudentSchedule("Kniesche, Niklas");
     }
 }
